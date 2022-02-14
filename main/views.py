d4f9d856f85eed696a17243fb68dfc6250abe76c
MountainManJosh <jlw1491625@hotmail.com>
2022-02-13 20:56:19 -0800

    django1

---
+++ main/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+
+# Create your views here.
+def home(requests):
+   return render(requests, "main/home.html", {})
+
+def review(requests):
+   return
+
+def reviews(requests):
+   return
